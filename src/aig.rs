4af7e731316508d50611abe04ea3eac07914f6c0
yanqingz <36521188+yanqingz@users.noreply.github.com>
2025-05-07 14:35:28 -0700

    Update aig.rs

--- src/aig.rs
+++ src/aig.rs
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 //! And-inverter graph format
 //!
 //! An AIG is derived from netlistdb synthesized in AIGPDK.
