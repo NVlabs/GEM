df6eaa06de26ae8bd8c70e0901a82de856c91c31
yanqingz <36521188+yanqingz@users.noreply.github.com>
2025-05-07 14:36:14 -0700

    Update pe.rs

--- src/pe.rs
+++ src/pe.rs
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 //! Partition executor
 
 use crate::aig::{DriverType, AIG, EndpointGroup};
